c8afc37e8e4753f556598099b30822df0e2ed838
Halit KAPLAN <halit_kaplan@ogrenci.karabuk.edu.tr>
2021-03-28 17:03:58 +0300

    19. Gün Angular Giriş 4

--- src/app/components/cart-summary/cart-summary.component.ts
+++ src/app/components/cart-summary/cart-summary.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { CartItem } from 'src/app/models/cartItem';
 import { Product } from 'src/app/models/product';
 import { CartService } from 'src/app/services/cart.service';
@@ -12,7 +13,8 @@ export class CartSummaryComponent implements OnInit {
 
   cartItems:CartItem[]=[];
   constructor(
-      private cartService:CartService
+      private cartService:CartService,
+      private toastrService:ToastrService
 
   ) { }
 
@@ -26,6 +28,8 @@ export class CartSummaryComponent implements OnInit {
 
   removeFromCart(product:Product){
     this.cartService.removeFromCart(product);
+    this.toastrService.error("Silindi", product.productName + " Sepetten silindi.");
+    
   }
 
 }
